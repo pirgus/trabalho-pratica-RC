a1577ec28eb681928b0c435cf01fec56f54c0818
pirgus <jaquecavaller@gmail.com>
2023-07-13 08:30:36 -0300

    Atualizacao pare-e-espere sem timer

--- UDPSender.cpp
+++ UDPSender.cpp
@@ -6,7 +6,7 @@
 #include <fstream>
 #include <cstring>
 
-const int BLOCK_TO_SEND = 1000;
+const int BLOCK_TO_SEND = 100;
 const int TIMEOUT_S = 5;
 
 int main(int argc, const char** argv){
@@ -54,69 +54,24 @@ int main(int argc, const char** argv){
 
 
         // -------------- garantia de entrega
-        // Configurar o temporizador
-        timeval timeout{};
-        timeout.tv_sec = TIMEOUT_S;
-        timeout.tv_usec = 0;
-
-        // Configurar o conjunto de descritores de leitura
-        fd_set readSet;
-        FD_ZERO(&readSet);
-        FD_SET(udpSocket, &readSet);
-
-        // Aguardar até que haja dados disponíveis para leitura ou ocorra um timeout
-        int readyDescriptors = select(udpSocket + 1, &readSet, nullptr, nullptr, &timeout);
-        if (readyDescriptors == -1) {
-            std::cerr << "Erro na função select()" << std::endl;
-            close(udpSocket);
-            return 1;
-        } else if (readyDescriptors == 0) {
-            std::cout << "Tempo limite atingido. Nenhuma resposta do servidor." << std::endl;
+        ssize_t received_bytes = recvfrom(udpSocket, buffer, sizeof(buffer), 0, (sockaddr*)&serverAddress, &serverAddressLength);
+        std::string receivedMessage(buffer, received_bytes);
+        //std::cout << "Mensagem recebida: " << receivedMessage << std::endl;
+        //int compare = strcmp(receivedMessage.c_str(), message);
+        // std::cout << "Valor da comparaçao = " << compare << std::endl;
+        if(received_bytes > 0){
+            // std::cout << "Pacote UDP " << count << " enviado com sucesso" << std::endl;
+            count++;
+            readed_bytes += BLOCK_TO_SEND;
+        }
+        else{
+            // pacote nao foi enviado corretamente
+            // std::cout << "Nao foi possível enviar o pacote, tentando novamente...\n";
+            // volta o ponteiro do arquivo para no reinício do laço tentar enviar de novo
             size_t go_back = size_t(file_to_send.tellg()) - BLOCK_TO_SEND;
             file_to_send.seekg(go_back);
+            count++;
         }
-
-        // Verificar se o socket está pronto para leitura
-        if (FD_ISSET(udpSocket, &readSet)) {
-            // Ler a resposta do servidor
-            char buffer[BLOCK_TO_SEND];
-            sockaddr_in serverResponse{};
-            socklen_t serverResponseLength = sizeof(serverResponse);
-            ssize_t receivedBytes = recvfrom(udpSocket, buffer, sizeof(buffer) - 1, 0, (sockaddr*)&serverResponse, &serverResponseLength);
-            if (receivedBytes < 0) {
-                //std::cerr << "Falha ao receber a resposta UDP" << std::endl;
-                std::cout << "Nao foi possível enviar o pacote, tentando novamente...\n";
-                // volta o ponteiro do arquivo para no reinício do laço tentar enviar de novo
-                size_t go_back = size_t(file_to_send.tellg()) - BLOCK_TO_SEND;
-                file_to_send.seekg(go_back);
-            }
-            else{
-                // Null-terminate o buffer para exibir a resposta como uma string
-                buffer[receivedBytes] = '\0';
-                // Exibir a resposta do servidor
-                std::cout << "Resposta do servidor: " << buffer << std::endl;
-                count++;
-                readed_bytes += BLOCK_TO_SEND;
-            }
-        }
-        
-        // ssize_t received_bytes = recvfrom(udpSocket, buffer, sizeof(buffer), 0, (sockaddr*)&serverAddress, &serverAddressLength);
-        // std::string receivedMessage(buffer, received_bytes);
-        // //std::cout << "Mensagem recebida: " << receivedMessage << std::endl;
-        // //int compare = strcmp(receivedMessage.c_str(), message);
-        // // std::cout << "Valor da comparaçao = " << compare << std::endl;
-        // if(received_bytes > 0){
-        //     // std::cout << "Pacote UDP " << count << " enviado com sucesso" << std::endl;
-        //     count++;
-        //     readed_bytes += BLOCK_TO_SEND;
-        // }
-        // else{
-        //     // pacote nao foi enviado corretamente
-        //     // std::cout << "Nao foi possível enviar o pacote, tentando novamente...\n";
-        //     // volta o ponteiro do arquivo para no reinício do laço tentar enviar de novo
-        //     size_t go_back = size_t(file_to_send.tellg()) - BLOCK_TO_SEND;
-        //     file_to_send.seekg(go_back);
-        // }
         // -------------------------------------
     }
 
